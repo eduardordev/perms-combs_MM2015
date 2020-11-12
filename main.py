0a90f3e73169b5caa94dd91d8d7490b96a88d207
Martín España <martineduardoer00@gmail.com>
2020-11-12 17:09:21 -0600

    Se agregó programación defensiva

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #PROYECTO FINAL
 #EDUARDO RAMÍREZ HERRERA #19946
 #MARTIN ESPAÑA #19258
+
 import itertools
 from itertools import *
 
@@ -20,20 +21,67 @@ while(state1):
 
     state2 = True
     count = 0 #contador para llenar el conjunto
-
-    max = int(input("Escriba la cantidad maxima de elementos en su conjunto: "))
-    print("------------------------------------------")
+    
+    ##########################################################################################
+    #Se solicita al usuario el número de elementos de su conjunto y se verifica que sea válido
+    ##########################################################################################
+    
+    maxIncorrect = True
+    
+    while maxIncorrect:
+        try:
+            max = int(input("Escriba la cantidad maxima de elementos en su conjunto: "))
+            if max > 0:
+                print("------------------------------------------")
+                maxIncorrect = False
+            elif max <= 0:
+                print("Debe ingresar un número no negativo mayor que 0...")
+        except ValueError:
+            print("Debe ingresar un número...")
+    
+    ##########################################################################################
 
     a = [] #Lista contenedora del conjunto
 
     #Bucle while para llenar el conjunto
     while count < max:
-        n = input("Ingrese una letra o un numero a su conjunto: ")
+        n = input("Ingrese un elemento (letras o numeros) a su conjunto: ")
         a.append(n)
         count += 1
     print("------------------------------------------")
+    
+    ####################################################################
+    #Se solicita al usuario el valor de k y se verifica que sea correcto
+    ####################################################################
+    
+    kIncorrect = True
 
-    k = int(input("Escriba la cantidad k de elementos que desea seleccionar del conjunto: "))
+    while kIncorrect:
+        try:
+            k = int(input("Escriba la cantidad k de elementos que desea seleccionar del conjunto (-1 para mostrar todos):"))
+        
+            #El numero k es mayor o igual a 0 pero menor que el numero de elementos
+            if k >= 0 and k < len(a):
+                kIncorrect = False
+            
+            #El numero k es igual a (-1)
+            elif k == -1:
+                k = len(a)
+                kIncorrect = False
+            
+            #El numero k es menor que (-1)
+            elif k < -1:
+                print("El número \"k\" debe ser mayor o igual a (-1)")
+            
+            #El numero k es mayor que el numero de elementos
+            elif k > len(a):
+                print("El número k debe ser menor o igual que el numero de elementos...")
+                
+        #El numero k no es una letra
+        except ValueError:
+            print("Debe ingresar un valor de \"k\" válido...")
+    
+    ####################################################################
 
     print("------------------------------------------")
     print("Los valores del conjunto son: ")
@@ -63,7 +111,7 @@ while(state1):
 
             print("------------------------------------------")
 
-        if opt == 2:
+        elif opt == 2:
             print("***** COMBINACIONES *****\n")
 
             cmb = list(itertools.combinations(a, k))
@@ -76,7 +124,7 @@ while(state1):
             print(cc)
             print("------------------------------------------")
 
-        if opt == 3:
+        elif opt == 3:
             print("***** PERMUTACIONES Y COMBINACIONES *****\n")
 
             print("PERMUTACIONES: ")
@@ -103,7 +151,7 @@ while(state1):
 
             print("------------------------------------------")
 
-        if opt == 4:
+        elif opt == 4:
             state2 = False
 
     exit = input("Si desea salir del programa presione la tecla (Y) (o presione enter para continuar con un nuevo conjunto): ")
@@ -112,4 +160,4 @@ while(state1):
     if exit == "y" or exit == "Y":
         print("***** FIN DEL PROGRAMA *****")
         state1 = False
-        break
+
